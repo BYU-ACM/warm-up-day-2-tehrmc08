fccb38d683cfda40f00c2d830fd5657d0943eede
Ryan Keck <tehrmc08@gmail.com>
2017-01-11 13:15:00 -0700

    RD

--- sol.py
+++ sol.py
@@ -1,16 +1,30 @@
 def Binary_Search(arr, to_find):
-  """A direct implementation of Newton's Method
+    """A direct implementation of Newton's Method
      (For sanity assume that func and func_prime define there own division correctly,
      that is, don't cast anything to a float)
      params: arr (a list ordered from least to greatest)
              to_find (the item to find in arr)
      returns: index (int), x, s.t. arr[x] == to_find
               None(none-type) if for all x, arr[x] != to_find
-  """
-  pass
+    """
+    cont = True
+    low = 0
+    high = len(arr)-1
+    while(cont):
+        mid = (low + high)/2
+        if(arr[mid] < to_find):
+            high = mid
+        elif(arr[mid] > to_find):
+            low = mid
+        else:
+            return mid
+        if(high==low):
+            return None
+    
+    pass
 
 def Bisection(func, left_side, right_side, tol=1e-5):
-  """A direct implementation of Newton's Method
+    """A direct implementation of Newton's Method
      (For sanity assume that func and func_prime define there own division correctly,
      that is, don't cast anything to a float)
      params: func (a function)
@@ -19,5 +33,20 @@ def Bisection(func, left_side, right_side, tol=1e-5):
              tol (a value for which the function should return once a value at least that distance from zero is found)
      returns: root (float), x, s.t. abs(func(x))<tol
               None(none-type) if func(left_side), func(right_side) < 0 or func(left_side), func(right_side) > 0
-  """
-  pass
+    """
+    if(func(left_side) < 0 and func(right_side) < 0):
+        return None
+    if(func(left_side) > 0 and func(right_side) > 0):
+        return None
+    cont = True
+    low = left_side
+    high = right_side
+    while(cont):
+        mid = (low + high)/2
+        if(func(mid) < -tol):
+            low = mid
+        elif(func(mid) > tol):
+            high = mid
+        else:
+            return mid
+    pass
